e1a76571d6d2c0c838baa815b9a2218d30785952
Roukaiya-sketch <roukaiyasheik@gmail.com>
2021-04-22 00:46:38 +0400

    [initial commit]

---
+++ src/mathWithFun.c
@@ -0,0 +1,11 @@
+#include "mathWithFun.h"
+
+int add(int a, int b)
+{
+	return a+b;
+}
+float mul(float a, float b)
+{
+	return a * b;
+}
+
---
+++ src/mathwithfun.h
@@ -0,0 +1,14 @@
+/*
+This header is named mathWithFun.h
+
+this file define some mathematical functions
+
+this header gather all the prototype needed for the script mathWithFun.c
+*/
+
+//here is a fonction to add two int
+int add(int a, int b); 
+
+// here is a function to multiply to float
+float mul(float a, float b);
+
